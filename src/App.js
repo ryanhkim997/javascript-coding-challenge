8ea035dcc86ba46051e9a00549cfed9a07361c65
Ryan <ryan@dks-macbook-pro.home>
2020-06-05 19:43:10 +0900

    Add API call for users and store data in App
    For now, rendering stringified data in App.
    
    Running into problem that useEffect() is being called indefinitely on every re-render (performance inhibitor), so the component needs a value of comparison so that the API call for users is only made once.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import './App.css';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const callAPI = async () => {
-    return Promise.resolve("Hello from Steezy 👋");
+  const callAPI = () => {
+    return fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .catch(err => console.error(err))
   };
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await callAPI();
+        console.log(response);
         setData(response);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
-  });
+  }, [ data ]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{data}</p>
+        <p>{JSON.stringify(data)}</p>
       </header>
     </div>
   );
